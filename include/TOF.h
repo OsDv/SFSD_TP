d90279299640d0b7d96428b3efa555bb7beb5a40
Oussama <oussamammm7@gmail.com>
2024-12-09 18:38:36 +0100

    TOF_writeLineTOlog

--- include/TOF.h
+++ include/TOF.h
@@ -12,16 +12,14 @@
 #define MAX_NAME_SIZE  20
 #define MAX_CITY_NAME  20
 #define DATE_SIZE 8
-#define StudentSize  sizeof(Student)
 #define MAX_RECORDS  100
-#define TOF_blockSize  sizeof(TOF_Block)
-#define TOF_HEADER_SIZE  sizeof(TOF_Header )
 #define LOADING_FACTOR 0.60
+#define TOF_PRINT_LINE_NUMBER_WIDTH 3
+#define TOF_PRINT_N_RW_WIDTH 3
 
-typedef struct {
-    FILE *file;
-    TOF_Header header;
-} TOF_FILE;
+extern int TOF_NUMBER_OF_READS;
+extern int TOF_NUMBER_OF_WRITES;
+
 typedef struct {
 	int id;
 	char firstName[MAX_NAME_SIZE];
@@ -29,6 +27,7 @@ typedef struct {
 	char birthDate[DATE_SIZE];
 	char birthCity[MAX_CITY_NAME];
 } Student;
+#define StudentSize  sizeof(Student)
 
 
 typedef struct {
@@ -37,32 +36,34 @@ typedef struct {
 	int NR;
 	int ND;
 } TOF_Block,TOF_Buffer;
+#define TOF_blockSize  sizeof(TOF_Block)
 
 typedef struct {
 	int NB;// number of blocks in the file
 	int NR;//number of records in the file
 	int ND;// number of deleted records in the file
 } TOF_Header;
-
+#define TOF_HEADER_SIZE sizeof(TOF_Header)
 enum TOF_LINE_STATUS {
 	EMPTY_LINE , MISSING_ID , MISSING_FIRST_NAME , MISSING_LAST_NAME , MISSING_BIRTH_DATE , MISSING_BIRTH_CITY
 };
+
+enum INSERT_STATUS {INSERT_SUCCUSFUL , RECORD_EXISTS , NOT_INSERTED};
 typedef struct {
-	FILE *f;
-	TOF_Header header;
-}
+    FILE *file;
+    TOF_Header header;
+} TOF_FILE;
+int TOF_setHeader(TOF_FILE *f , TOF_Header *header);
 
-int TOF_setHeader(FILE *f , TOF_Header *header);
-
-int TOF_getHeader(FILE *f , TOF_Header *header);
-
-
-int TOF_readBlock(FILE * f , int n , TOF_Buffer *buffer) ;
-
-int TOF_writeBlock(FILE * f , int n , TOF_Buffer *buffer);
+int TOF_getHeader(TOF_FILE *f , TOF_Header *header);
 
 
-int TOF_search(FILE *f , int key , bool *found , int i , int j , Student *student);
+int TOF_readBlock(TOF_FILE * f , int n , TOF_Buffer *buffer) ;
+
+int TOF_writeBlock(TOF_FILE * f , int n , TOF_Buffer *buffer);
+
+
+int TOF_search(TOF_FILE *f , int key , bool *found , int *i , int *j , Student *student);
 
 
 
--- src/TOF.c
+++ src/TOF.c
@@ -1,28 +1,58 @@
-#include "C:\Users\adlen\OneDrive\Documents\GitHub\SFSD_TP\include\TOF.h"
-
-
-
-
-int TOF_setHeader(FILE *f , TOF_Header *header){
-	fseek(f , 0 , SEEK_SET );
-	fwrite(header , TOF_HEADER_SIZE , 1,f);
+#include <TOF.h>
+int TOF_NUMBER_OF_READS;
+int TOF_NUMBER_OF_WRITES;
+int TOF_open(const char *name , TOF_FILE *file , char mode){
+    switch (mode)
+    {
+    case 'n':
+    file->file= fopen(name,"wb+");
+    file->header.NB=0;
+    file->header.ND=0;
+    file->header.NR=0;
+        break;
+    case 'r':
+    file->file= fopen(name,"rb");
+	fseek(file->file , 0 , SEEK_SET );
+	fread(&(file->header) , TOF_HEADER_SIZE , 1,file->file);
+        break;
+    case 'a':
+    file->file= fopen(name,"rb+");
+	fseek(file->file , 0 , SEEK_SET );
+	fread(&(file->header) , TOF_HEADER_SIZE , 1,file->file);        
+        break;
+    
+    default:
+        break;
+    }
 }
-int TOF_getHeader(FILE *f , TOF_Header *header){
-	fseek(f , 0 , SEEK_SET);
-	fread(header , TOF_HEADER_SIZE , 1,f);
+
+int TOF_close(TOF_FILE *file){
+	fseek(file->file , 0 , SEEK_SET );
+	fwrite(&(file->header) , TOF_HEADER_SIZE , 1,file->file);    
+    fclose(file->file);
+    file->file=NULL;
 }
 
-int TOF_readBlock(FILE * f , int n , TOF_Buffer *buffer) {
-	fseek(f , TOF_HEADER_SIZE+TOF_blockSize*(n-1) , SEEK_SET);
-	fread(buffer ,TOF_blockSize , 1 , f); 
+int TOF_setHeader(TOF_FILE *f, TOF_Header *header){
+	(*header)=f->header;
+}
+int TOF_getHeader(TOF_FILE *f , TOF_Header *header){
+	(*header)=f->header;
 }
 
-int TOF_writeBlock(FILE * f , int n , TOF_Buffer *buffer){
-	fseek(f ,  TOF_HEADER_SIZE+TOF_blockSize*(n-1), SEEK_SET);
-	fwrite(buffer ,TOF_blockSize,1, f);
+int TOF_readBlock(TOF_FILE *f, int n , TOF_Buffer *buffer) {
+	fseek(f->file , TOF_HEADER_SIZE+TOF_blockSize*(n-1) , SEEK_SET);
+	fread(buffer ,TOF_blockSize , 1 , f->file); 
+    TOF_NUMBER_OF_READS++;
 }
 
-int TOF_search(FILE *f , int key , bool *found , int *i , int *j , Student *student){
+int TOF_writeBlock(TOF_FILE * f , int n , TOF_Buffer *buffer){
+	fseek(f->file ,  TOF_HEADER_SIZE+TOF_blockSize*(n-1), SEEK_SET);
+	fwrite(buffer ,TOF_blockSize,1, f->file);
+    TOF_NUMBER_OF_WRITES++;
+}
+
+int TOF_search(TOF_FILE *f , int key , bool *found , int *i , int *j , Student *student){
 	TOF_Header header;
 	TOF_Buffer buffer;
 	TOF_getHeader(f,&header);
@@ -53,10 +83,10 @@ int TOF_search(FILE *f , int key , bool *found , int *i , int *j , Student *stud
 
         } else if(key < buffer.data[0].id){
             // the element should be found in the inferior half of the file
-                sup = i-1;
+                sup = (*i)-1;
         } else {
             // the element should be found in the superior half of the file
-            inf = i + 1;
+            inf = (*i) + 1;
         }
         // search ended and element not founded should be inserted in block i index j
         if (inf > sup) stop = true ;
@@ -65,7 +95,7 @@ int TOF_search(FILE *f , int key , bool *found , int *i , int *j , Student *stud
 
 
 
-InsertionStatus insertElement(FILE *f , elm_t e){
+enum INSERT_STATUS insertElement(TOF_FILE *f , Student e){
     int i,j;
     bool found;
     TOF_Buffer buffer,nextBuffer;
@@ -73,7 +103,7 @@ InsertionStatus insertElement(FILE *f , elm_t e){
     TOF_Header header;
     TOF_getHeader(f,&header);
     TOF_search(f,e.id,&found,&i,&j,NULL);
-    if (found) return -1;   //Already exist
+    if (found) return RECORD_EXISTS;   //Already exist
     TOF_readBlock(f,i,&buffer);
     bool stop = false;
     while (!stop){
@@ -120,8 +150,43 @@ InsertionStatus insertElement(FILE *f , elm_t e){
 
    
     TOF_setHeader(f, &header);
-    return 0; // Successful insertion
+    return INSERT_SUCCUSFUL; // Successful insertion
 }
 
+void TOF_writeLineToLog(FILE *f , int lineNumber , enum TOF_LINE_STATUS lineS , enum INSERT_STATUS insertS){{
+    if (insertS==INSERT_SUCCUSFUL){
+        fprintf(f,"+%*d:VALIDE_LINE:INSERTED:%5dR %5dW\n",TOF_PRINT_LINE_NUMBER_WIDTH,lineNumber,TOF_NUMBER_OF_READS,TOF_NUMBER_OF_WRITES);
+    } else {
+        switch (lineS)
+        {
+        case EMPTY_LINE:
+            fprintf(f,"*%*d:NOT-INSERTED:EMPTY-LINE:%4dR %4dW\n",TOF_PRINT_LINE_NUMBER_WIDTH,lineNumber,TOF_NUMBER_OF_READS,TOF_NUMBER_OF_WRITES);
+            break;
+        
+        case VALID_LINE:
+            fprintf(f,"*%*d:NOT-INSERTED:DUPLICAT:%4dR %4dW\n",TOF_PRINT_LINE_NUMBER_WIDTH,lineNumber,TOF_NUMBER_OF_READS,TOF_NUMBER_OF_WRITES);
+            break;
+        case MISSING_ID:
+            fprintf(f,"*%*d:NOT-INSERTED:MISSING_ID:%4dR %4dW\n",TOF_PRINT_LINE_NUMBER_WIDTH,lineNumber,TOF_NUMBER_OF_READS,TOF_NUMBER_OF_WRITES);
+            break;
+        case MISSING_BIRTH_CITY:
+            fprintf(f,"*%*d:NOT-INSERTED:MISSING_BIRTH_CITY:%4dR %4dW\n",TOF_PRINT_LINE_NUMBER_WIDTH,lineNumber,TOF_NUMBER_OF_READS,TOF_NUMBER_OF_WRITES);
+            break;
+        case MISSING_FIRST_NAME:
+            fprintf(f,"*%*d:NOT-INSERTED:MISSING_FIRST_NAME:%4dR %4dW\n",TOF_PRINT_LINE_NUMBER_WIDTH,lineNumber,TOF_NUMBER_OF_READS,TOF_NUMBER_OF_WRITES);
+            break;
+        case MISSING_LAST_NAME:
+            fprintf(f,"*%*d:NOT-INSERTED:MISSING_LAST_NAME:%4dR %4dW\n",TOF_PRINT_LINE_NUMBER_WIDTH,lineNumber,TOF_NUMBER_OF_READS,TOF_NUMBER_OF_WRITES);
+            break;
+        case MISSING_BIRTH_DATE:
+            fprintf(f,"*%*d:NOT-INSERTED:MISSING_BIRTH_DATE:%4dR %4dW\n",TOF_PRINT_LINE_NUMBER_WIDTH,lineNumber,TOF_NUMBER_OF_READS,TOF_NUMBER_OF_WRITES);
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
 
+}
 int TOF_creatFile()
